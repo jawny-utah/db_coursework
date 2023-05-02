fe674814f8c8a2db5f18f05c2a929a7370ca1cda
jawny-utah <dsheva@daisybill.com>
2023-05-02 23:24:13 +0300

    Add basic edit/create/delete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def fields_to_exclude
+    ["id", "created_at", "updated_at"]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,24 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "homepages#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :homepages, only: :index do
+    collection do
+      get :administrators
+      get :cities
+      get :clients_subscriptions
+      get :clients
+      get :orders
+      get :reviews
+      get :scooters_in_work
+      get :scooters
+      get :subscriptions
+      get :transactions
+    end
+  end
+
+  resources :administrators
+  resources :clients
+  resources :subscriptions
+  resources :scooters
+  resources :cities
 end
